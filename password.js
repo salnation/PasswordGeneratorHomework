2d3d8d12f74f2a5faa5b30d9e063e9fb12b822fb
Stefan Salituro <stefansalituro@gmail.com>
2020-06-10 15:29:37 -0400

    The variables that I will need to define have been listed - now I have to figure out what formulas to use to correctly define the variables.

--- password.js
+++ password.js
@@ -14,6 +14,14 @@ document.getElementById(document).ready(function(); {
     //value is an integer between 0 and -1
     random = Math.floor(random)
     //value is an integer between 1 and max
-    random =  + 1;
+    random = + 1;
 }
+
+//next variables to be set - characters, password, begin, stop, char, num
+
+var characters = ""
+var password = ""
+var begin = ""
+var stop = ""
+var char = ""
 
